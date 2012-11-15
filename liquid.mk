150835a8411ee79f6b8574dc347b145619394444
nocoast <voteforpedro8@gmail.com>
2012-11-14 22:12:09 -0600

    Liquified

---
+++ liquid.mk
@@ -0,0 +1,25 @@
+# GSM
+$(call inherit-product, vendor/liquid/config/common_gsm.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := n7100
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
+# Phone 
+$(call inherit-product, vendor/liquid/config/common_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/n7100/full_n7100.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := n7100
+PRODUCT_NAME := liquid_n7100
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := GT-N7100
+PRODUCT_MANUFACTURER := samsung
+
+# Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=t03gxx TARGET_DEVICE=t03g BUILD_FINGERPRINT="samsung/t03gxx/t03g:4.1.1/JRO03C/N7100XXALJ3:user/release-keys" PRIVATE_BUILD_DESC="t03gxx-user 4.1.1 JRO03C N7100XXALJ3 release-keys"
